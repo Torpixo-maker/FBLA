83e6534e061c72e07259dfa56e7bbea62ec52d10
darthpancake2 <jaredpowang@gmail.com>
2025-11-20 17:54:41 -0500

    "can you save on github rn"

--- fbla/car_minigame.gd
+++ fbla/car_minigame.gd
@@ -1,10 +1,15 @@
 extends Node2D
 var old_tire_bolts = "on"
+var new_tire_bolts = "off"
 var hand_on_wrench = false
 var wrench_on_old_wheel = false
+var wrench_on_new_wheel = false
 var hand_on_old_wheel = false
+var hand_on_new_wheel = false
 var handState = "hand"
 var wrench_in_hand = false
+var old_tire_on_car = true
+var new_tire = "off"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +22,7 @@ func _process(delta: float) -> void:
 		show()
 	else:
 		hide()
+
+
+func _on_car_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
--- fbla/node_2d.gd
+++ fbla/node_2d.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var hand1 = preload ("res://Sprites/hand-1.png")
-var hand2 = 	preload ("res://Sprites/hand-2.png")
+var hand1 = preload ("res://hand-1.png")
+var hand2 = 	preload ("res://hand-2.png")
 
 
 # Called when the node enters the scene tree for the first time.
