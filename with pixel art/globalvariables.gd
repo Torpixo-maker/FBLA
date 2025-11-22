f9780af0e35ab5881380a6019d33b6c418a46c45
Torpixo-maker <yidazhao10@gmail.com>
2025-11-21 20:07:33 -0500

    Reapply "Merge branch 'main' of https://github.com/Torpixo-maker/FBLA"
    This reverts commit fa62ce7d8e8ff31f7f1c25cd14b3279fa865dfc8.

---
+++ with pixel art/globalvariables.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var gameStarted = false
+var inMinigames = false
+var tasksLeft = 3
+var camera_position_x = []
+var camera_position_y = []
+var minigame = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
